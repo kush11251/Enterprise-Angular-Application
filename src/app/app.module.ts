04e4c763aec9c3d61ff7d0b0a550ab86664e0fb2
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 18:04:42 +0530

    feat: integrate src/app/app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+// app module
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RouterModule } from './app/app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
